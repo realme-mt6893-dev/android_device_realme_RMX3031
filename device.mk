f476caf2d09ae011c9308c00225930c45a40146e
Himanshu Tyagi <himanshut567@gmail.com>
2023-02-28 10:08:21 +0530

    Revert "RMX3031: Rename audio service HAL"
    This reverts commit e5e67e5ffd2441638c90c394fca5286a73be259e.

--- device.mk
+++ device.mk
@@ -42,7 +42,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio.service.mediatek \
+    android.hardware.audio.service \
     android.hardware.audio@6.0-impl \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
